b160b6878bd088f93aafd97cf2a7cb8ed3baceb3
lyzChange <liyazhou@ysten.com>
2018-11-23 14:36:32 +0800

    hongxing2

---
+++ hongxingjiedao2.0/js/stuScript.js
@@ -0,0 +1,20 @@
+var box=document.getElementById('box');
+		document.onkeydown=function(e){
+		var t = parseFloat(box.style.top) || box.offsetTop;
+			if(t<0){
+				if(e.keyCode==38){//上
+				box.style.top= t + 100 +"px";
+				// alert(t)
+				
+			}
+			}
+			if(t>-400){
+				if(e.keyCode==40){//下
+				box.style.top= t - 100 +"px";
+				
+			}
+			}
+		if(e.keyCode=="27"){
+			window.close();
+		}
+	}
